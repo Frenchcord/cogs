2624b1462136eda13282cf5a262b2fedfab4013e
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-25 11:51:41 -0400

    Update main.py

--- example/main.py
+++ example/main.py
@@ -2,3 +2,7 @@ import frenchcord
 bot = frenchcord.Robot('Token')
 bot.process_folder('cogs', logs=True, excepte=['in_dev'])
 bot.run(['?'])
+'''
+One line bot method
+import frenchcord; bot = frenchcord.Robot('token'); bot.process_folder('cogs', logs=True, excepte=['in_dev']); bot.run(['?'])
+'''
